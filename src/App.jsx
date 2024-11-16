2ed9f3e576a6eb7d32ed7b670367991717e1c9cf
Ajmal Ansari Ahamed Mohammed <sardarahmed110@gmail.com>
2024-11-16 10:29:17 +0530

    just setting up the reqired variables

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [bookedSeats, setBookedSeats] = useState({})
+  const sectionsData = [
+    { name: "VIP", rows: 5, seatsPerRow: 5 },
+    { name: "General", rows: 6, seatsPerRow: 6 },
+    { name: "Economy", rows: 8, seatsPerRow: 8 }
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+
+    </div>
   )
 }
 
